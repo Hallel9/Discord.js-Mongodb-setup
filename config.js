fef492a9b416348485495905f7759153acbe8166
Hallel9 <47569515+Hallel9@users.noreply.github.com>
2021-05-02 12:22:52 -0400

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  token: "ODM3ODc0OTcwNDMyMTEwNjUz.YIy53w.lzCpYlI2I3Lk7KcrR3Mnk928xuI",
-  prefix: "f?",
-  mongo_path:
-    "mongodb+srv://dbHallel:hallelhallel@cluster0.xg2l8.mongodb.net/firecam-db?retryWrites=true&w=majority",
-};
