9e416679f10551ec7abc6f95809ac87a341b2b5d
Hallel9 <47569515+Hallel9@users.noreply.github.com>
2021-05-02 12:59:56 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -29,39 +29,4 @@ module.exports = (client) => {
       });
     });
   }, ms("15 minutes"));
-
-  // Express section
-
-  const clientDetails = {
-    guilds: client.guilds.cache.size,
-    users: client.users.cache.size,
-    channels: client.channels.cache.size,
-  };
-
-  const path = require("path");
-
-  const express = require("express");
-
-  const app = express();
-
-  const port = 3000 || 3001;
-
-  app.set("view engine", "ejs");
-
-  app.get("/", (req, res) => {
-    res
-      .status(200)
-      .sendFile(path.join(__dirname, "..", "pages", "landingPage.html"));
-  });
-
-  app.get("/commands", (req, res) => {
-    const commands = getCommands();
-    res.status(200).render("commands", { commands });
-  });
-
-  app.get("/Info", (req, res) => {
-    res.status(200).send(clientDetails);
-  });
-
-  app.listen(port);
 };
